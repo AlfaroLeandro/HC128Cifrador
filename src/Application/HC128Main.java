6ae0ea3b256e7ef2d0a5025002f7a67c7e22baf7
Leandro <being.leeaa15@gmail.com>
2023-06-10 10:45:19 -0300

    todo acomodado

--- src/Application/HC128Main.java
+++ src/Application/HC128Main.java
@@ -19,6 +19,7 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
@@ -40,6 +41,7 @@ public class HC128Main extends JFrame {
 	private JTextField txt_iv;
 	private JTextField txt_Image;
 	private File imageSelectedFile;
+	private File imageEncryptedFile;
 	private final JSeparator separator = new JSeparator();
 
 	/**
@@ -66,7 +68,7 @@ public class HC128Main extends JFrame {
 		setResizable(false);
 		setTitle("Algoritmo HC-128");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1200, 1400);
+		setBounds(100, 100, 1200, 600);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(172, 251, 231));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -74,7 +76,7 @@ public class HC128Main extends JFrame {
 		contentPane.setLayout(null);
 		
 		txt_Key = new JTextField();
-		txt_Key.setBounds(96, 74, 262, 20);
+		txt_Key.setBounds(96, 77, 262, 20);
 		contentPane.add(txt_Key);
 		txt_Key.setColumns(10);
 		
@@ -87,7 +89,7 @@ public class HC128Main extends JFrame {
 				txt_Key.setText(DataGenerator.generateData());
 			}
 		});
-		btn_GenerateKey.setBounds(377, 71, 118, 23);
+		btn_GenerateKey.setBounds(377, 77, 118, 23);
 		contentPane.add(btn_GenerateKey);
 		
 		JLabel lbl_Key = new JLabel("Key:");
@@ -97,11 +99,11 @@ public class HC128Main extends JFrame {
 		
 		JLabel lbl_iv = new JLabel("IV:");
 		lbl_iv.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lbl_iv.setBounds(26, 117, 60, 14);
+		lbl_iv.setBounds(26+600, 77, 60, 14);
 		contentPane.add(lbl_iv);
 		
 		txt_iv = new JTextField();
-		txt_iv.setBounds(96, 114, 262, 20);
+		txt_iv.setBounds(96+600, 77, 262, 20);
 		contentPane.add(txt_iv);
 		txt_iv.setColumns(10);
 		
@@ -114,21 +116,21 @@ public class HC128Main extends JFrame {
 				txt_iv.setText(DataGenerator.generateData());
 			}
 		});
-		btn_GenerateIv.setBounds(377, 111, 118, 23);
+		btn_GenerateIv.setBounds(377+600, 77, 118, 23);
 		contentPane.add(btn_GenerateIv);
 		
 		JLabel lbl_Image = new JLabel("Imagen:");
 		lbl_Image.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lbl_Image.setBounds(26, 156, 60, 14);
+		lbl_Image.setBounds(26+330, 156-10, 60, 14);
 		contentPane.add(lbl_Image);
 		
 		txt_Image = new JTextField();
-		txt_Image.setBounds(96, 153, 262, 20);
+		txt_Image.setBounds(96+330, 153-10, 262, 20);
 		contentPane.add(txt_Image);
 		txt_Image.setColumns(10);
 		
 		JLabel lbl_OriginalImage = new JLabel("");
-		lbl_OriginalImage.setBounds(56, 306, 174, 172);
+		lbl_OriginalImage.setBounds(56, 306-10, 174, 172);
 		contentPane.add(lbl_OriginalImage);
 
 		JLabel lbl_UnlamImage = new JLabel("");
@@ -141,17 +143,26 @@ public class HC128Main extends JFrame {
 		
 		JLabel lbl_Original = new JLabel("Imagen original");
 		lbl_Original.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lbl_Original.setBounds(85, 270, 112, 14);
+		lbl_Original.setBounds(85, 270-10, 112, 14);
 		contentPane.add(lbl_Original);
 		
 		JLabel lbl_EncryptedImage = new JLabel("");
-		lbl_EncryptedImage.setBounds(282, 295, 174, 172);
+		lbl_EncryptedImage.setBounds(282+180, 295-10, 174, 172);
 		contentPane.add(lbl_EncryptedImage);
 		
-		JLabel lbl_Encrypted = new JLabel("Imagen encriptada");
+		JLabel lbl_Encrypted = new JLabel("Imagen cifrada");
 		lbl_Encrypted.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lbl_Encrypted.setBounds(319, 270, 118, 14);
+		lbl_Encrypted.setBounds(319+180, 270-10, 118, 14);
 		contentPane.add(lbl_Encrypted);
+		
+		JLabel lbl_DecryptedImage = new JLabel("");
+		lbl_DecryptedImage.setBounds(282+585, 295-10, 174, 172);
+		contentPane.add(lbl_DecryptedImage);
+		
+		JLabel lbl_Decrypted = new JLabel("Imagen descifrada");
+		lbl_Decrypted.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lbl_Decrypted.setBounds(319+585, 270-10, 118, 14);
+		contentPane.add(lbl_Decrypted);
 		
 		Button btn_SelectImage = new Button("Seleccionar");
 		btn_SelectImage.setBackground(new Color(0, 128, 128));
@@ -187,56 +198,60 @@ public class HC128Main extends JFrame {
 				
 			}
 		});
-		btn_SelectImage.setBounds(377, 150, 118, 23);
+		btn_SelectImage.setBounds(377+330, 150-10, 118, 23);
 		contentPane.add(btn_SelectImage);
 		
-		Button btn_Encrypt = new Button("Encriptar / Desencriptar");
+		Button btn_Encrypt = new Button("Cifrar");
 		btn_Encrypt.setFont(new Font("Dialog", Font.BOLD, 12));
 		btn_Encrypt.setForeground( new Color(255, 255, 255) );
 		btn_Encrypt.setBackground(new Color(0, 128, 128));
 		btn_Encrypt.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(txt_Key.getText().length() == 16) 
-				{
-					if(txt_iv.getText().length() == 16) 
-					{
-						if (imageSelectedFile != null) 
-						{
-							try 
-							{
-								CipherManager cm = new CipherManager();
-								cm.initializeData(txt_Key.getText(), txt_iv.getText());
-								File encryptedFile = cm.encrypt(imageSelectedFile);
-								BufferedImage imageEncrypted;
-								imageEncrypted = ImageIO.read(encryptedFile);
-								ImageIcon imageLabel = new ImageIcon(imageEncrypted.getScaledInstance(lbl_OriginalImage.getWidth(), lbl_OriginalImage.getHeight(), Image.SCALE_DEFAULT));
-								lbl_EncryptedImage.setIcon(imageLabel);
-							}
-							catch(Exception ex) 
-							{
-								JOptionPane.showMessageDialog(null, "Error encriptando la imagen, por favor intentelo de nuevo: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-							}
-						}
-						else 
-						{
-							JOptionPane.showMessageDialog(null, "Por favor seleccione una imagen", "Error", JOptionPane.ERROR_MESSAGE);
-						}
-					}
-					else 
-					{
-						JOptionPane.showMessageDialog(null, "El tama�o del iv debe ser de 16 caracteres", "Error", JOptionPane.ERROR_MESSAGE);
-					}
-				}
-				else 
-				{
-					JOptionPane.showMessageDialog(null, "El tama�o de la key debe ser de 16 caracteres", "Error", JOptionPane.ERROR_MESSAGE);
-				}		
+				encrypt(lbl_OriginalImage, lbl_EncryptedImage);
 			}
 		});
-		btn_Encrypt.setBounds(130, 191, 180, 23);
+		btn_Encrypt.setBounds(130+120, 191+120, 180, 23);
 		contentPane.add(btn_Encrypt);
-		separator.setBounds(0, 239, 520, 31);
+		
+		Button btn_Encrypt_Intercept = new Button("Cifrar interceptando 1bit");
+		btn_Encrypt_Intercept.setFont(new Font("Dialog", Font.BOLD, 12));
+		btn_Encrypt_Intercept.setForeground( new Color(255, 255, 255) );
+		btn_Encrypt_Intercept.setBackground(new Color(0, 128, 128));
+		btn_Encrypt_Intercept.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				encrypt(lbl_OriginalImage, lbl_EncryptedImage, true);
+			}
+		});
+		btn_Encrypt_Intercept.setBounds(130+120, 191+120+50, 180, 23);
+		contentPane.add(btn_Encrypt_Intercept);
+		
+		Button btn_Decrypt_Intercept = new Button("Descifrar interceptando 1bit");
+		btn_Decrypt_Intercept.setFont(new Font("Dialog", Font.BOLD, 12));
+		btn_Decrypt_Intercept.setForeground( new Color(255, 255, 255) );
+		btn_Decrypt_Intercept.setBackground(new Color(0, 128, 128));
+		btn_Decrypt_Intercept.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				decrypt(lbl_EncryptedImage, lbl_DecryptedImage, true);
+			}
+		});
+		btn_Decrypt_Intercept.setBounds(130+540, 191+120+50, 180, 23);
+		contentPane.add(btn_Decrypt_Intercept);
+		
+		
+		separator.setBounds(0, 239-50, 1200, 31);
 		contentPane.add(separator);
+		
+		Button btn_Decrypt = new Button("Descifrar");
+		btn_Decrypt.setFont(new Font("Dialog", Font.BOLD, 12));
+		btn_Decrypt.setForeground( new Color(255, 255, 255) );
+		btn_Decrypt.setBackground(new Color(0, 128, 128));
+		btn_Decrypt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				decrypt(lbl_EncryptedImage, lbl_DecryptedImage);
+			}
+		});
+		btn_Decrypt.setBounds(130+540, 191+120, 180, 23);
+		contentPane.add(btn_Decrypt);
 		
 		JLabel lblCriptografa = new JLabel(" Criptograf\u00EDa");
 		lblCriptografa.setFont(new Font("Tahoma", Font.BOLD, 11));
@@ -246,5 +261,112 @@ public class HC128Main extends JFrame {
 		JLabel lblAlgoritmoHc = new JLabel(" Algoritmo HC-128");
 		lblAlgoritmoHc.setBounds(79, 30, 134, 14);
 		contentPane.add(lblAlgoritmoHc);
+	}
+	
+	public void encrypt(JLabel lbl_OriginalImage, JLabel lbl_EncryptedImage) {
+		encrypt(lbl_OriginalImage, lbl_EncryptedImage, false);
+	}
+	
+	public void encrypt(JLabel lbl_OriginalImage, JLabel lbl_EncryptedImage, boolean interceptText) {
+		if(txt_Key.getText().length() == 16) 
+		{
+			if(txt_iv.getText().length() == 16) 
+			{
+				if (imageSelectedFile != null) 
+				{
+					try 
+					{
+						String key = txt_Key.getText();
+						if(interceptText) {
+							char[] chars = key.toCharArray();
+							int len = chars.length;
+							Random rand = new Random();
+							int indexRand = rand.nextInt(0, len);
+							chars[indexRand] = (char) (chars[indexRand] ^ 1 ); //cambio un bit en la pos 1
+							key = new String(chars);
+							System.out.println("llave cambiada a: " + key);
+						}
+						
+						CipherManager cm = new CipherManager();
+						cm.initializeData(key, txt_iv.getText());
+						File encryptedFile = cm.encrypt(imageSelectedFile);
+						BufferedImage imageEncrypted;
+						imageEncrypted = ImageIO.read(encryptedFile);
+						ImageIcon imageLabel = new ImageIcon(imageEncrypted.getScaledInstance(lbl_OriginalImage.getWidth(), lbl_OriginalImage.getHeight(), Image.SCALE_DEFAULT));
+						lbl_EncryptedImage.setIcon(imageLabel);
+					}
+					catch(Exception ex) 
+					{
+						JOptionPane.showMessageDialog(null, "Error encriptando la imagen, por favor intentelo de nuevo: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+					}
+				}
+				else 
+				{
+					JOptionPane.showMessageDialog(null, "Por favor seleccione una imagen", "Error", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+			else 
+			{
+				JOptionPane.showMessageDialog(null, "El tama�o del iv debe ser de 16 caracteres", "Error", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+		else 
+		{
+			JOptionPane.showMessageDialog(null, "El tama�o de la key debe ser de 16 caracteres", "Error", JOptionPane.ERROR_MESSAGE);
+		}		
+	}
+	
+	public void decrypt(JLabel lbl_EncryptedImage, JLabel lbl_DecryptedImage) {
+		decrypt(lbl_EncryptedImage, lbl_DecryptedImage, false);
+	}
+	
+	public void decrypt(JLabel lbl_EncryptedImage, JLabel lbl_DecryptedImage, boolean interceptText) {
+		if(txt_Key.getText().length() == 16) 
+		{
+			if(txt_iv.getText().length() == 16) 
+			{
+				if (imageSelectedFile != null) 
+				{
+					try 
+					{
+						String key = txt_Key.getText();
+						if(interceptText) {
+							char[] chars = key.toCharArray();
+							int len = chars.length;
+							Random rand = new Random();
+							int indexRand = rand.nextInt(0, len);
+							chars[indexRand] = (char) (chars[indexRand] ^ 1 ); //cambio un bit en la pos 1
+							key = new String(chars);
+							System.out.println("llave cambiada a: " + key);
+						}
+						
+						imageEncryptedFile = new File("Imagenes\\temp.bmp");
+						CipherManager cm = new CipherManager();
+						cm.initializeData(key, txt_iv.getText());
+						File encryptedFile = cm.encrypt(imageEncryptedFile);
+						BufferedImage imageEncrypted;
+						imageEncrypted = ImageIO.read(encryptedFile);
+						ImageIcon imageLabel = new ImageIcon(imageEncrypted.getScaledInstance(lbl_EncryptedImage.getWidth(), lbl_EncryptedImage.getHeight(), Image.SCALE_DEFAULT));
+						lbl_DecryptedImage.setIcon(imageLabel);
+					}
+					catch(Exception ex) 
+					{
+						JOptionPane.showMessageDialog(null, "Error encriptando la imagen, por favor intentelo de nuevo: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+					}
+				}
+				else 
+				{
+					JOptionPane.showMessageDialog(null, "Por favor seleccione una imagen", "Error", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+			else 
+			{
+				JOptionPane.showMessageDialog(null, "El tama�o del iv debe ser de 16 caracteres", "Error", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+		else 
+		{
+			JOptionPane.showMessageDialog(null, "El tama�o de la key debe ser de 16 caracteres", "Error", JOptionPane.ERROR_MESSAGE);
+		}		
 	}
 }
